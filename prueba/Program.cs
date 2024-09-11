59cb18f0ba2278b090ca703f0e54bb2e76dcb187
Jesus Duran <jesusduran141014@gmail.com>
2024-09-11 11:36:25 +0200

    cambio 3

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,3 +1,12 @@
-Console.Write("congratulations ");
-Console.Write("");
-Console.Write("you wrote your firts code");
+var Name = "Bob";
+var messagesnumber = 3;
+var temperature = 34.4;
+Console.Write("Hello, ");
+Console.Write(Name);
+Console.Write("! You have  ");
+Console.Write(messagesnumber);
+Console.Write(" messagess in your inbox. the temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
+
+
