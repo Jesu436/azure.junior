7de32b304bda613163f1714d1bc06174846fa2e1
Jesus Duran <jesusduran141014@gmail.com>
2024-09-12 12:53:38 +0200

    conversion grados

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -9,6 +9,21 @@ value-= 8;
 Console.WriteLine(value);
 value*= 85;
 Console.WriteLine(value);
+value/= 70;
+Console.WriteLine(value);
+Console.Clear();
+
+var values = 1;
+values++;
+Console.WriteLine("first: " + values);
+Console.WriteLine($"second: {value++}");
+
+var fahrenheit = 94;
+decimal celsius = (fahrenheit - 32) * 5/9;
+Console.WriteLine("The temperetaure in celsius is: " + Math.Round(celsius,1) + " Celsius");
+Console.WriteLine("The temperetaure in celsius is: " + celsius.ToString("0.0") + " Celsius");
+
+
 
 
 
