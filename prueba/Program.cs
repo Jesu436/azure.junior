1ac9cbfa28327d7b61d7872e98b670109392c558
Jesus Duran <jesusduran141014@gmail.com>
2024-09-13 11:04:44 +0200

    tabla 2

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,24 +1,10 @@
+using System.Collections;
 using System.Text;
-Console.Clear();
-string[] cursos = {"English 101", "Algebra 101", "Biology 101", "Computer science 1", "Psycologi 101" };
-double[] calificaciones = {4.0, 3.0, 3.0, 3.0, 4.0};
-int[] creditos = {3, 3, 4, 4, 3};
-double sumacalificacionesponderadas = 0;
-int sumacreditos = 0;
-for (int i = 0; i < cursos.Length; i++)
+var dados = new Random();
+for(int i = 0; i < 15; i++)
 {
-    sumacalificacionesponderadas += calificaciones[i] * creditos[i];
-    sumacreditos += creditos[i];
-    }
-
-double gpa = sumacalificacionesponderadas / sumacreditos;
-
-Console.WriteLine("Estudiante: Sophia Bulga");
-Console.WriteLine("Curso\t\t\tNota\tHoras Credito");
-for (int i = 0; i < cursos.Length; i++)
-{
-    Console.WriteLine($"{cursos[i],-20}\t{calificaciones[i]}\t{creditos[i]}");
-    Console.WriteLine($"\nGPA Final: {gpa:F2}");
+    var roll = dados.Next(1, 7);
+    Console.WriteLine(roll);
 }
 
 
