5a4e5e5a33422f137304401176310b51ab531ba6
Jesus Duran <jesusduran141014@gmail.com>
2024-09-16 13:54:11 +0200

    foreach 3

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System.Text;
 Console.Clear();
 string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
-
+// Hacer un bucle para llamar nombres que empiecen por B
 foreach (string orderID in orderIDs)
 {
     if (orderID.StartsWith("B"))
