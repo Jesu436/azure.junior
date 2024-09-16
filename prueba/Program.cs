9a6a27bc11084572c340f6053441695135744b62
Jesus Duran <jesusduran141014@gmail.com>
2024-09-16 11:49:11 +0200

    for each

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,30 +1,11 @@
 using System.Collections;
+using System.Globalization;
 using System.Text;
-Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
-if (daysUntilExpiration >= 10)
+string [] carros = { "lambo", "buga", "merce",};
+foreach (var name in (carros))
 {
-    Console.WriteLine("Your suscription will expire soon renew now!");
-}    
-else if (daysUntilExpiration >= 5)
-{
-    Console.WriteLine($"Your suscription will expire in {daysUntilExpiration} days. " + $"Renew now to enjoy a {discountPercentage}% of discount!.");
-    discountPercentage = 10;
+    Console.WriteLine(name);
 }
-
-else if (daysUntilExpiration == 1)
-{
-    discountPercentage = 20;
-    Console.WriteLine("Your suscription expires withting a day!" + $" Renew now to enjoy a {discountPercentage}% of discount!.");
-}
-else 
-{
-    Console.WriteLine("Your suscription has expired.");
-}
-
-
-
 
 
 
