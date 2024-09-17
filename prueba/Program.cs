04d832e2d10b5aa56af4f8abc404581735253b52
Jesus Duran <jesusduran141014@gmail.com>
2024-09-17 12:34:19 +0200

    switch

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,68 +1,62 @@
-using System;
-using System.Linq;
-class Program
+using System.Text;
+
+
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
 {
-    static void Main()
-    {
-        string[] alumnos = {"Sophia", "Andrew", "Emma", "Logan"};
+    case "01":
+    type = "Sweat shirt";
+    break;
+    case "02":
+    type = "T-shirt";
+    break;
+    case "03":
+    type = "Sweat pants";
+    break;
+    default:
+    type = "Others";
+    break;
 
-
-        int[][] notas = {
-            new int[] {90, 86, 87, 98, 100},
-            new int[] {92, 89, 81, 96, 90},
-            new int[] {90, 85, 87, 98, 68},
-            new int[] {90, 95, 87, 88, 96},
-        };
-
-        foreach (var alumno in alumnos.Select((nombre, indice)  => new { nombre, indice}))
-        {
-            int[] notasAlumno = notas[alumno.indice];
-            int sumaNotas = 0;
-            int creditosAdicionales = 0;
-
-
-            foreach (var nota in notasAlumno)
-            {
-                if (nota <= 10)
-                {
-                    creditosAdicionales += nota;
-            
-                }
-                else
-                {
-                    sumaNotas += nota;
-                }
-            }
-
-            double notaFinal = (sumaNotas + (creditosAdicionales * 10)) / 5.0;
-            string calificacion = ObtenerCalificacion(notaFinal);
-
-            Console.WriteLine($"{alumno.nombre}: Nota final = {notaFinal:F2}, Calificacion = {calificacion}");
-
-        }
-        Console.WriteLine("Press the Enter key to continue");
-        Console.ReadLine();
-
-    }
-
-    static string ObtenerCalificacion(double nota)
-    {
-        if (nota >= 97) return "A+";
-        else if (nota >= 93) return "A";
-        else if (nota >= 90) return "A-";
-        else if (nota >= 87) return "B+";
-        else if (nota >= 83) return "B";
-        else if (nota >= 80) return "B-";
-        else if (nota >= 77) return "C+";
-        else if (nota >= 73) return "C";
-        else if (nota >= 70) return "C-";
-        else if (nota >= 67) return "D+";
-        else if (nota >= 63) return "D";
-        else if (nota >= 60) return "D-";
-        else return "F";
-    }
-    
 }
 
+switch (product[1])
+{
+    case "BL":
+    color = "Black";
+    break;
+    case "MN":
+    color = "Maroon";
+    break;
+    default:
+    color = "White";
+    break;
 
 
+}
+
+switch (product[2])
+{
+    case "S":
+    size = "Small";
+    break;
+    case "M":
+    size = "Medium";
+    break;
+    case "L":
+    size = "Large";
+    break;
+    default:
+    size = "One size fits all";
+    break;
+
+}
+Console.WriteLine($"Product: {size} {color} {type}");
