6f9dea61e7181ca67b667f361d1f7820271b8dcb
Jesus Duran <jesusduran141014@gmail.com>
2024-09-17 13:37:51 +0200

    permisos con condicionales

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,6 +1,34 @@
 using System.Text;
+string permission = "Admin|Manager";
+int level = 53;
 
-int saleAmount = 900;
-// int discount = saleAmount > 1000 ? 100 : 50;
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
 
-Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+
+
