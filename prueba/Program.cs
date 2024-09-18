b69894eb870b8a8a51d765f8457ba5534812954a
Jesus Duran <jesusduran141014@gmail.com>
2024-09-18 10:31:28 +0200

    regiones

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,35 +1,10 @@
 using System.Text;
-string permission = "Admin|Manager";
-int level = 53;
-
-if (permission.Contains("Admin"))
+#region Compilacion de condicion
+if(true)
 {
-    if (level > 55)
-    {
-        Console.WriteLine("Welcome, Super Admin user.");
-    }
-    else
-    {
-        Console.WriteLine("Welcome, Admin user.");
-    }
-}
-else if (permission.Contains("Manager"))
-{
-    if (level >= 20)
-    {
-        Console.WriteLine("Contact an Admin for access.");
-    }
-    else
-    {
-        Console.WriteLine("You do not have sufficient privileges.");
-    }
-}
-else
-{
-    Console.WriteLine("You do not have sufficient privileges.");
-}
+    Console.WriteLine("Hola mundo");
 
-// buscar la forma mas sencilla de hacer esto
-
+}
+#endregion
 
 
