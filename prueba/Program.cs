83cf446f317e944249e44e2f89959ecd4de74dcb
Jesus Duran <jesusduran141014@gmail.com>
2024-09-18 12:58:13 +0200

    juego de pelea

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,15 +1,22 @@
 using System.Text;
-for (int i = 0; i < 101; i++)
+Random dice = new Random();
+int Heroe = 10;
+int Monstruo = 10;
+do
 {
-   if ((i % 3 == 0) && (i % 5 == 0))
-   
-      Console.WriteLine($"{i} - FizzBuzz");
-      else if ((i % 3 == 0))
-      Console.WriteLine($"{i} - Buzz");
-      else if (i % 5 == 0)
-      Console.WriteLine($"{i} - Fizz");
-      else
-      Console.WriteLine($"{i}");
-      
-}
+   Console.ReadKey();
+   int roll = dice.Next(1, 11);
+   Monstruo -= roll;
+   Console.WriteLine($"El heroe ataca y causa " + roll + " Puntos de daño");
+   Console.WriteLine("Salud del monstruo " + Monstruo);
 
+   if (Monstruo <= 0) continue;
+   roll = dice.Next(1, 11);
+   Heroe -= roll;
+   Console.WriteLine($"El Monstruo ataca y causa " + roll + " Puntos de daño");
+   Console.WriteLine($"Salud del Heroe " + Heroe);
+   
+
+} while (Heroe > 0 && Monstruo > 0 );
+
+Console.WriteLine(Heroe > Monstruo ? "Heroe Gana" : "Monstruo Gana");
