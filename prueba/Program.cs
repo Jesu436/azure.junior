3a60d743a04482ef1e6c42ccb0497d118f305de2
Jesus Duran <jesusduran141014@gmail.com>
2024-09-18 13:40:19 +0200

    tipos de flotantes

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,25 +1,10 @@
 using System.Text;
-Random dice = new Random();
-int Heroe = 10;
-int Monstruo = 10;
-var roll = 0;
-var isHero = true;
+Console.WriteLine("Signed integral types:");
 
-do
-{
-   roll = dice.Next(1, 11);
-   if (isHero)
-   {Monstruo -= roll;
-   Console.WriteLine($"El montruo ha recibido {roll} puntos de daño. Y le quedan {Monstruo} puntos de vida");
-   }
-   else
-   {
-      Heroe -= roll;
-      Console.WriteLine($"El heroe ha recibido {roll} puntos de daño. y le quedan {Heroe} puntos de vida");
-   }
-   isHero = !isHero;
-   
-} while (Heroe > 0 && Monstruo > 0);
-Console.WriteLine($"El combate ha finalizado, ha ganado {(Heroe > 0 ? "Heroe" : "Monstruo")}");
-
-
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+Console.WriteLine($"long   : {double.MinValue} to {double.MaxValue}");
+Console.WriteLine($"long   : {float.MinValue} to {float.MaxValue}");
+Console.WriteLine($"long   : {decimal.MinValue} to {decimal.MaxValue}");
