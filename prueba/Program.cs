73316a34c748d51c81731c845067268120aba15b
Jesus Duran <jesusduran141014@gmail.com>
2024-09-19 11:51:32 +0200

    bucle para conversion

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,39 +1,16 @@
 using System.Text;
 
-int first = 2;
-string second = "4";
-string result = first + second;
-Console.WriteLine(result);
+string[] values = { "12,3", "45", "ABC", "11", "DEF" };
+var total = 0.0;
+var message = "";
 
-var uno = 2;
-var dos = "4";
-var resultado = uno + int.Parse(dos);
-Console.WriteLine(resultado);
-
-
-try
+foreach (var value in values)
 {
-    string firs = "2";
-    string secon = "4";
-    int sum = int.Parse(firs) + int.Parse(secon);
-    Console.WriteLine(sum);
-
+    double number;
+    if (double.TryParse(value, out number))
+        {total += number;}
+        else
+        {message += value;}
 }
-catch (System.Exception)
-{
-    Console.WriteLine("numero no valido");
-}
-
-
-string fir = "2";
-string seco = "4";
-int sumas; 
-Console.WriteLine(int.TryParse (seco, out sumas));
-int suman = int.Parse(fir) + sumas;
-Console.WriteLine(suman);
-
-
-string value1 = "5";
-string value2 = "7";
-int resultados = Convert.ToInt32(value1) * Convert.ToInt32(value2);
-Console.WriteLine(resultados);
+Console.WriteLine("mensaje: " + message);
+Console.WriteLine("total: " + total);
