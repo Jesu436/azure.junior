b3a3891367da16819df7a243d061aa9fd7a40732
Jesus Duran <jesusduran141014@gmail.com>
2024-09-19 13:38:01 +0200

    voltear palabras

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,13 +1,20 @@
 using System.Text;
-int[] pallets = { 4, 1, 2, 3 };
-Console.WriteLine("");
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new string(valueArray);
+string resulto = string.Join("," ,valueArray);
+Console.WriteLine(resulto);
 
-Array.Clear(pallets, 0, 2);
-Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
 
-foreach (var pallet in pallets)
+string pangram = "The quick brown fox jumps over the lazy dog";
+string[] message = pangram.Split(' ');
+string[] newMessage = new string[message.Length];
+for (int i = 0; i < message.Length; i++)
 {
-    Console.WriteLine($"-- " + pallet);
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    newMessage[i] = new string(letters);
 }
-
-
+string result = String.Join(" ", newMessage);
+Console.WriteLine(result);
