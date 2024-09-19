cfe0241c142fd32fc3092f7e7195556aae2fc1ca
Jesus Duran <jesusduran141014@gmail.com>
2024-09-19 11:32:45 +0200

    convercion

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,6 +1,39 @@
 using System.Text;
-Console.WriteLine("");
-Console.WriteLine("Floating point types:");
-Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+int first = 2;
+string second = "4";
+string result = first + second;
+Console.WriteLine(result);
+
+var uno = 2;
+var dos = "4";
+var resultado = uno + int.Parse(dos);
+Console.WriteLine(resultado);
+
+
+try
+{
+    string firs = "2";
+    string secon = "4";
+    int sum = int.Parse(firs) + int.Parse(secon);
+    Console.WriteLine(sum);
+
+}
+catch (System.Exception)
+{
+    Console.WriteLine("numero no valido");
+}
+
+
+string fir = "2";
+string seco = "4";
+int sumas; 
+Console.WriteLine(int.TryParse (seco, out sumas));
+int suman = int.Parse(fir) + sumas;
+Console.WriteLine(suman);
+
+
+string value1 = "5";
+string value2 = "7";
+int resultados = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(resultados);
