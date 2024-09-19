d6399e0bfb3c5d3e385b1b432c0c06647fd60bc4
Jesus Duran <jesusduran141014@gmail.com>
2024-09-19 13:53:37 +0200

    el zorro

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,20 +1,13 @@
+using System.Runtime.CompilerServices;
 using System.Text;
-string value = "abc123";
-char[] valueArray = value.ToCharArray();
-Array.Reverse(valueArray);
-// string result = new string(valueArray);
-string resulto = string.Join("," ,valueArray);
-Console.WriteLine(resulto);
+string pangram = "The quick fox jumps over the lazy dog";
+string[] message = pangram.Split(" ");
+string [] reverse = new string[message.Length];
 
-
-string pangram = "The quick brown fox jumps over the lazy dog";
-string[] message = pangram.Split(' ');
-string[] newMessage = new string[message.Length];
 for (int i = 0; i < message.Length; i++)
 {
-    char[] letters = message[i].ToCharArray();
-    Array.Reverse(letters);
-    newMessage[i] = new string(letters);
-}
-string result = String.Join(" ", newMessage);
-Console.WriteLine(result);
+    string word = message[i];
+    string reverseword = string.Join("",word.ToCharArray().Reverse());
+    reverse[i] = reverseword;
+}  
+Console.WriteLine(string.Join(" ",reverse));
