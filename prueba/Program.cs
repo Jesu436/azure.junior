e69c25769d0b844c5fec75bf19799243385d40af
Jesus Duran <jesusduran141014@gmail.com>
2024-09-19 13:20:38 +0200

    pallets

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,12 +1,13 @@
 using System.Text;
-var value1 = 11;
-var value2 = 6.2;
-var value3 = 4.3;
+int[] pallets = { 4, 1, 2, 3 };
+Console.WriteLine("");
 
-var result1 = Convert.ToInt32(value1 / value2);
-Console.WriteLine($" El resultado de los valores dividido es {result1};");
-var result2 = (decimal)value2 / (decimal)value3;
-Console.WriteLine($"El resultado de los valores es {result2}");
-var result3 = (float)value3 / value1;
-Console.WriteLine($"El resultado de los valores es {result3}");
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- " + pallet);
+}
+
 
