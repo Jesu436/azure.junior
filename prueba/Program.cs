ce13a737f586da9ff8ace05fa628521168ebd11e
Jesus Duran <jesusduran141014@gmail.com>
2024-09-19 12:50:28 +0200

    divisiones con conversiones

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,16 +1,12 @@
 using System.Text;
+var value1 = 11;
+var value2 = 6.2;
+var value3 = 4.3;
 
-string[] values = { "12,3", "45", "ABC", "11", "DEF" };
-var total = 0.0;
-var message = "";
+var result1 = Convert.ToInt32(value1 / value2);
+Console.WriteLine($" El resultado de los valores dividido es {result1};");
+var result2 = (decimal)value2 / (decimal)value3;
+Console.WriteLine($"El resultado de los valores es {result2}");
+var result3 = (float)value3 / value1;
+Console.WriteLine($"El resultado de los valores es {result3}");
 
-foreach (var value in values)
-{
-    double number;
-    if (double.TryParse(value, out number))
-        {total += number;}
-        else
-        {message += value;}
-}
-Console.WriteLine("mensaje: " + message);
-Console.WriteLine("total: " + total);
