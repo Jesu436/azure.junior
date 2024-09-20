cd63baceac1aa104599952a912a9bbdd14941412
Jesus Duran <jesusduran141014@gmail.com>
2024-09-20 13:26:04 +0200

    ejercicio  en clase replace

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,22 +1,17 @@
 using System.Globalization;
 CultureInfo.CurrentCulture = new CultureInfo("en-US");
-string customerName = "Ms. Barros";
 
-string currentProduct = "Magic Yield";
-int currentShares = 2975000;
-decimal currentReturn = 0.1275m;
-decimal currentProfit = 55000000.0m;
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
-string newProduct = "Glorious Future";
-decimal newReturn = 0.13125m;
-decimal newProfit = 63000000.0m;
 
-Console.WriteLine($"Dear {customerName},\n");
-Console.WriteLine($"As avalued customer of our {currentProduct} offering, we are excited to introduce you to a new financial product that could significantly enhance your returns.\n");
-Console.WriteLine($"Currently, you own {currentShares:N} shares with a return {currentReturn:P2}%.\n");
-Console.WriteLine($"Our new product, {newProduct}, offers a return of {newReturn:P2}%. Given your current volume your potential profit will increase to ${newProfit}.\n");
-Console.WriteLine("Here`s a quick comparison\n");
-Console.WriteLine($"Magic Yield: {currentReturn:P2}% - €{currentProfit}".PadLeft(38));
-Console.WriteLine($"Glorious Future: {newReturn:P2}% - €{newProfit}");
-Console.WriteLine("We believe this new product could be a great fit for your investment strategy.\n");
-Console.WriteLine("Best regards,\n[Jesus Duran]");
+int start = input.IndexOf("<span>") + 6;
+int end = input.IndexOf("</span>");
+string quantity = input.Substring(start, end - start);
+string output = input.Replace("&trade;", "&reg;");
+output = output.Replace("<div>", "").Replace("</div>", "");
+Console.WriteLine("Quantity: " + quantity);
+Console.WriteLine("Output:" + output);
+
+
+
+
