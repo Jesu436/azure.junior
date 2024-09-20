70627dea6d11577b05f7bc960997a0542fbcf59f
Jesus Duran <jesusduran141014@gmail.com>
2024-09-20 13:56:24 +0200

    tiempo reflejado

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,17 +1,6 @@
 using System.Globalization;
 CultureInfo.CurrentCulture = new CultureInfo("en-US");
-
-const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
-
-
-int start = input.IndexOf("<span>") + 6;
-int end = input.IndexOf("</span>");
-string quantity = input.Substring(start, end - start);
-string output = input.Replace("&trade;", "&reg;");
-output = output.Replace("<div>", "").Replace("</div>", "");
-Console.WriteLine("Quantity: " + quantity);
-Console.WriteLine("Output:" + output);
-
-
-
+var hoy = new DateTime(2024, 05, 31);  
+Console.WriteLine(hoy.AddMonths(1).Ticks);
+TimeSpan today = TimeSpan.FromDays(36);
 
