68f7cf41ce69ac24e99d5d1ab641064f438001bf
Jesus Duran <jesusduran141014@gmail.com>
2024-10-01 11:16:47 +0200

    void

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,6 +1,27 @@
-using System.Globalization;
-CultureInfo.CurrentCulture = new CultureInfo("en-US");
-var hoy = new DateTime(2024, 05, 31);  
-Console.WriteLine(hoy.AddMonths(1).Ticks);
-TimeSpan today = TimeSpan.FromDays(36);
+string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
 
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+void DisplayEmails(string[,] names, string domain = "contoso.com")
+{
+    for (int i = 0; i < names.GetLength(0); i++)
+    {
+        string email = names[i, 0].Substring(0, 2) + names[i, 1];
+        email = email.ToLower();
+        Console.WriteLine($"{email}@{domain}");
+    }
+}
+
+DisplayEmails(corporate);
+DisplayEmails(external, externalDomain);
