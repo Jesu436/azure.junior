6cfa4a9747b98e0cf6cf882101609b0e3370a4bd
Jesus Duran <jesusduran141014@gmail.com>
2024-10-04 13:28:27 +0200

    testing bueno con todo 3

---
+++ prueba/calculadora.cs
@@ -0,0 +1,29 @@
+namespace prueba;
+
+public class Calculadora
+{
+    public int suma(int numero1, int numero2)
+    {
+        return numero1 + numero2;
+
+    }
+    public int resta(int numero1, int numero2)
+    {
+        return numero1 - numero2;
+
+    }
+     public int multiplicacion (int numero1, int numero2)
+    {
+        return numero1 * numero2;
+
+    }
+    public int division (int numero1, int numero2)
+    {
+        return numero1 / numero2;
+
+    }
+
+
+
+    
+}
